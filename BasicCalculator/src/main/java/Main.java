777a645ed007224bbf8c5fa35211c73c9fa625a1
Tyson Hall <tn8938@gmail.com>
2023-10-11 14:10:28 -0400

    updated Main.java class

--- BasicCalculator/src/main/java/Main.java
+++ BasicCalculator/src/main/java/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first number: ");
-        int firstNum = scanner.nextInt();
 
-        System.out.println("Enter the second number: ");
-        int secNum = scanner.nextInt();
+        double inputValue1 =
+                requestDoubleInput(scanner);
+
+        double inputValue2 =
+                requestDoubleInput(scanner);
 
         System.out.println("""
                 Possible calculations:
@@ -18,27 +19,46 @@ public class Main {
                 (D)ivide
                 Please select an option:
                 """);
-        // charAt(0) means char at index 0
-        int option = scanner.next().charAt(0);
 
-        int answer;
-        switch (option) {
-            case 'A':
-                answer = firstNum + secNum;
-                System.out.println(firstNum + "+" + secNum + "=" + answer);
-                break;
-            case 'S':
-                answer = firstNum - secNum;
-                System.out.println(firstNum + "-" + secNum + "=" + answer);
-                break;
-            case 'M':
-                answer = firstNum * secNum;
-                System.out.println(firstNum + "*" + secNum + "=" + answer);
-                break;
-            case 'D':
-                answer = firstNum / secNum;
-                System.out.println(firstNum + "/" + secNum + "=" + answer);
-                break;
+        double result;
+        boolean done = false;
+
+        while (!done) {
+            // charAt(0) means char at index 0
+            char option = scanner.next().charAt(0);
+
+            switch (option) {
+                case 'A', 'a':
+                    result = inputValue1 + inputValue2;
+                    System.out.println(inputValue1 + " + " + inputValue2 + " = " + result);
+                    done = true;
+                    break;
+                case 'S', 's':
+                    result = inputValue1 - inputValue2;
+                    System.out.println(inputValue1 + " - " + inputValue2 + " = " + result);
+                    done = true;
+                    break;
+                case 'M', 'm':
+                    result = inputValue1 * inputValue2;
+                    System.out.println(inputValue1 + " * " + inputValue2 + " = " + result);
+                    done = true;
+                    break;
+                case 'D', 'd':
+                    result = inputValue1 / inputValue2;
+                    System.out.println(inputValue1 + " / " + inputValue2 + " = " + result);
+                    done = true;
+                    break;
+                default:
+                    System.out.println("I didn't understand that, please select an option again");
+            }
         }
+    }
+
+    private static double requestDoubleInput(Scanner scanner) {
+        double inputValue = 0.0;
+        System.out.println("Please enter a floating point number: ");
+        inputValue = scanner.nextDouble();
+        scanner.nextLine();
+        return inputValue;
     }
 }
